17147b6cd2181437ff9018a46175bd5e2dff8ad3
Denys Sttrilchuk <denia876@gmail.com>
2024-04-20 14:57:52 +0300

    lesson-2, done HW (try with method stat and ternary operator).

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-// Створити папку "baseFolder". В ній створити 5 папок, в кожній з яких створити по 5 файлів з розширенням txt.
-// Вивести в консоль шляхи до кожного файлу чи папки, також вивести поряд інформацію про те, чи є це файл чи папка.
+const http = require('node:http');
 
-const fsPromises = require('node:fs/promises');
-const path = require('node:path');
+
+const users = [
+    { id: 1, name:'Maksym', email:'feden@gmail.com', password:'we123'},
+    { id: 2, name: 'Iryna', email: 'iryna@example.com', password: 'qwerty' },
+    { id: 3, name: 'Oleh', email: 'oleg@example.com', password: 'password123' },
+    { id: 4, name: 'Anna', email: 'anna@example.com', password: 'annapass' },
+    { id: 5, name: 'Petro', email: 'petro@example.com', password: 'petro123' },
+    { id: 6, name: 'Kateryna', email: 'katya@example.com', password: 'katya456' },
+    { id: 7, name: 'Vitaliy', email: 'vitaliy@example.com', password: 'vitalikpass' },
+    { id: 8, name: 'Natalia', email: 'natalya@example.com', password: 'natalyapass' },
+    { id: 9, name: 'Ihor', email: 'igor@example.com', password: 'igor789' },
+    { id: 10, name: 'Yulia', email: 'yulia@example.com', password: 'yuliapass' }
+]
 
 async function foo() {
     try {
-        for (let i = 1; i < 6; i++) {
-            const folderPath = path.join(__dirname, 'baseFolder', `file${i}`);
-            await fsPromises.mkdir(folderPath, {recursive:true});
-            const statsFolder = await fsPromises.stat(folderPath);
-            statsFolder.isDirectory() ? console.log(`${folderPath} - It is a folder`) : console.log(`${folderPath} - It is not folder`);
-
-            // console.log(`${folderPath} - is a folder`);
-            for (let j = 1; j < 6; j++) {
-                const filePath = path.join(folderPath, `text${j}.txt`);
-                await fsPromises.writeFile(filePath, `Some content ${j}`);
-                const statsFile = await fsPromises.stat(filePath);
-                statsFile.isFile() ? console.log(`${filePath} - It is a file`) : console.log(`${filePath} - It is not a file`);
-
-                // console.log(`${filePath} - is a file`);
+        const server = http.createServer((req,res)=>{
+            if (req.method === 'GET' && req.url === '/users') {
+                res.end(JSON.stringify(users));
             }
-        }
-    }catch (e) {
+            res.end('Hello from Node.js');
+        });
+        server.listen(3000,'0.0.0.0', ()=>{
+            console.log('server is running at http://0.0.0.0:3000/')
+        })
+    } catch (e) {
         console.error(e)
     }
 }
