4db723ceed84cfef2620aa50bf94ec316fadeca9
Denys Sttrilchuk <denia876@gmail.com>
2024-04-25 12:54:48 +0300

    lesson-5 (mongoose, env)

---
+++ src/models/user.model.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+import { RoleEnum } from "../enums/role.enum";
+import { IUser } from "../user.interface";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: RoleEnum.USER },
+    isDeleted: { type: Boolean, default: false },
+    isVerified: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const User = mongoose.model<IUser>("users", userSchema);
